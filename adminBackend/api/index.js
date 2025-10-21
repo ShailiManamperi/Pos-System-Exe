577f64c5b4740bc90e6a9dfc715747fcc8742ed3
Shailimanamperi <shailimanamperii2002@gmail.com>
2025-10-21 13:03:56 +0530

    update with vercel

---
+++ adminBackend/api/index.js
@@ -0,0 +1,7 @@
+// /api/index.js
+import serverless from "serverless-http";
+import app from "../server.js";
+
+// Export the Express app as a serverless function for Vercel
+export const handler = serverless(app);
+export default handler;
