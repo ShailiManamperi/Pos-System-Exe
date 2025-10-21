0a7d8695e8c718e41f57ebab7b68e8e2363ca122
Shailimanamperi <shailimanamperii2002@gmail.com>
2025-10-21 12:47:54 +0530

    update vercel deploy

--- adminBackend/api/auth.js
+++ adminBackend/api/auth.js
@@ -1,10 +1,14 @@
-import auth from "../Routes/auth.js";
 import express from "express";
 import serverless from "serverless-http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import authRoutes from "../Routes/auth.js";
 
 const app = express();
-app.use(express.json());
-app.use("/api/auth", auth);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/auth", authRoutes);
 
 export const handler = serverless(app);
 export default handler;
--- adminBackend/api/main.js
+++ adminBackend/api/main.js
@@ -1,9 +1,13 @@
-import mainRoutes from "../Routes/mainRoutes.js";
 import express from "express";
 import serverless from "serverless-http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mainRoutes from "../Routes/mainRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use("/api/admin/main", mainRoutes);
 
 export const handler = serverless(app);
